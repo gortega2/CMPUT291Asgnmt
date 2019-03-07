d680776273116eb907b7d5cc63ff645a948cb642
gortega2 <35354398+gortega2@users.noreply.github.com>
2019-03-06 20:39:26 -0700

    Task 3 function completed
    There is no error checking if user input is incorrect

--- 291Assignment3.py
+++ 291Assignment3.py
@@ -31,6 +31,30 @@ def InsertValues():
     connection.commit()
     
     
+def SelectFunction():
+    global connection, cursor
+    
+    print("Please choose a task to preform type e/exit to exit: \n 1. Task 1 \n 2. Task 2 \n 3. Task 3 \n 4. Task 4 \n 5. Task 5 \n 6. Task 6")
+
+    return input()
+
+def ExecuteFunction(choice):
+
+    if choice == 1:
+        Task1function()
+    elif choice == 2:
+        Task2function()
+    elif choice == 3:
+        Task3function()
+    elif choice == 4:
+        Task4function()
+    elif choice == 5:
+        Task5function()
+    elif choice == 6:
+        Task6function()
+
+    return
+    
 
 def Task1function():
     global connection, cursor
@@ -45,6 +69,14 @@ def Task2function():
 
 def Task3function():
     global connection, cursor
+    
+    string = input("Please enter a range by typing the lower bound number, a - and a higher bound number (Ex: 2-4))\n")
+    lower_bound = int(string[0])
+    upper_bound = int(string[2])
+    cursor.execute("SELECT COUNT(*), r.reviewer FROM users u, reviews r WHERE r.reviewer = u.email GROUP BY r.reviewer HAVING COUNT(*) >= :lb AND COUNT(*) <= :ub",
+              {"lb":lower_bound, "ub":upper_bound})
+    rows = cursor.fetchall()
+    print(rows)
 
     return
 
@@ -74,6 +106,11 @@ def main():
     connect(path)
     #createTables()    // Only needs to be run once to create Tables
     #InsertValues()    // Only needs to be run once to insert values
+    while True:
+        choice = SelectFunction()
+        if choice == 'e' or choice == 'exit':
+            break
+        ExecuteFunction(int(choice))
     
 
     connection.commit()
